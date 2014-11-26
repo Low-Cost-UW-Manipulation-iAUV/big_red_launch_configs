aecda70abf603d0dfcb6b2b355f68c3cb65d8af1
Raphael Nagel <compiaffe@users.noreply.github.com>
2014-11-26 18:37:48 +0000

    added copy script tp copy over the support stuff

---
+++ copy_support_stuff_bbb_brlnet.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "I'll copy all big_red_launch_configs files over to the BBB"
+
+rsync -avzh *.xml 								BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/
+rsync -avzh *.txt								BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/
+rsync -avzh ./launch/*.launch	 				BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/launch/
+rsync -avzh ./src/*.cpp							BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/src/
+rsync -avzh *.yaml		 						BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/
+rsync -avzh *.md								BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/
+rsync -avzh ./urdf/*.urdf						BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/urdf/
+
+rsync -avzh ../support_auv						BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/
+rsync -avzh ../support_messages					BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/
+rsync -avzh ../support_library					BBB_BRLWIFI:/home/ubuntu/uwesub_catkin_workspace/src/
+
+echo "All done, Good Success!"
