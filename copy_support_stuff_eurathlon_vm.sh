bbf6d7e9b66c87f641111969e2ecd15cd2999b8e
Raphael Nagel <compiaffe@users.noreply.github.com>
2014-11-28 22:52:41 +0000

    stopped normal big_red_launc_config files being send whenever we want to send the support stuff

--- copy_support_stuff_eurathlon_vm.sh
+++ copy_support_stuff_eurathlon_vm.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 echo "I'll copy all big_red_launch_configs files over to the BBB"
 
-rsync -avzh *.xml 								eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
-rsync -avzh *.txt								eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
-rsync -avzh ./launch/*.launch	 				eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/launch/
-rsync -avzh ./src/*.cpp							eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/src/
-rsync -avzh *.yaml		 						eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
-rsync -avzh *.md								eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
-rsync -avzh ./urdf/*.urdf						eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/urdf/
-
 rsync -avzh ../support_auv						eurathlon_vm:/home/euratlhon/uwesub_msc/src/
 rsync -avzh ../support_msgs						eurathlon_vm:/home/euratlhon/uwesub_msc/src/
 rsync -avzh ../support_library					eurathlon_vm:/home/euratlhon/uwesub_msc/src/
