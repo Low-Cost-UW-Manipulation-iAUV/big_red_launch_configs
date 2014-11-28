a4731b35dee1d9a8c2488fb2608755821be825cb
Raphael Nagel <compiaffe@users.noreply.github.com>
2014-11-28 22:39:58 +0000

    added new copy file

---
+++ copy_support_stuff_eurathlon_vm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "I'll copy all big_red_launch_configs files over to the BBB"
+
+rsync -avzh *.xml 								eurathlon_vm:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/
+rsync -avzh *.txt								eurathlon_vm:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/
+rsync -avzh ./launch/*.launch	 				eurathlon_vm:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/launch/
+rsync -avzh ./src/*.cpp							eurathlon_vm:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/src/
+rsync -avzh *.yaml		 						eurathlon_vm:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/
+rsync -avzh *.md								eurathlon_vm:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/
+rsync -avzh ./urdf/*.urdf						eurathlon_vm:/home/ubuntu/uwesub_catkin_workspace/src/big_red_launch_configs/urdf/
+
+rsync -avzh ../support_auv						eurathlon_vm:/home/ubuntu/uwesub_catkin_workspace/src/
+rsync -avzh ../support_messages					eurathlon_vm:/home/ubuntu/uwesub_catkin_workspace/src/
+rsync -avzh ../support_library					eurathlon_vm:/home/ubuntu/uwesub_catkin_workspace/src/
+
+echo "All done, Good Success!"
