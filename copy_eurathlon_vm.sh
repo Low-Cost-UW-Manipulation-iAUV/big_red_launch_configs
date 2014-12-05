d50bfafe4fa762b368074bb131ad8b6c9b136028
Raphael Nagel <compiaffe@users.noreply.github.com>
2014-12-05 19:54:13 +0000

    micron launchfiles now contain the velocity of sound

--- copy_eurathlon_vm.sh
+++ copy_eurathlon_vm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "I'll copy all ADIS_Interface files over to the BBB"
 
-rsync -avzh *.xml 								eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
-rsync -avzh ./launch/*.launch	 				eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/launch/
-rsync -avzh *.yaml		 						eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
-rsync -avzh *.md								eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
-rsync -avzh ./urdf/*.urdf						eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/urdf/
-rsync -avzh *.txt								eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs
-rsync -avzh ./src/*.cpp							eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/src/
-
+rsync -avzh ./src/*.cpp 							eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/src/
+rsync -avzh ./include/big_red_launch_configs/*.hpp 	eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/include/big_red_launch_configs/
+rsync -avzh CMakeLists.txt 							eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
+rsync -avzh *.xml 									eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
+rsync -avzh *.launch	 							eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
+rsync -avzh *.yaml		 							eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
+rsync -avzh *.md									eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
+rsync -avzh ./urdf/*.urdf							eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/urdf/
 
 echo "All done, Good Success!"
---
+++ copy_support_micron_driver_bmt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "I'll copy all ADIS_Interface files over to the BBB"
+
+rsync -avzh ../support_auv/micron_driver/src/*.cpp 								bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/src/
+rsync -avzh ../support_auv/micron_driver/include/micron_driver/*.hpp 			bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/include/micron_driver/
+rsync -avzh ../support_auv/micron_driver/CMakeLists.txt 						bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/
+rsync -avzh ../support_auv/micron_driver/*.xml 									bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/
+rsync -avzh ../support_auv/micron_driver/*.launch	 							bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/
+rsync -avzh ../support_auv/micron_driver/*.yaml		 							bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/
+rsync -avzh ../support_auv/micron_driver/*.md									bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/
+rsync -avzh ../support_auv/micron_driver/urdf/*.urdf							bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/urdf/
+
+echo "All done, Good Success!"
