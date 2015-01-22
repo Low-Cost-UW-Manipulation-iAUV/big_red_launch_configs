f40f77c4087325317c45361fd40b7d0c6e69db8a
Raphael Nagel <compiaffe@users.noreply.github.com>
2015-01-22 14:59:24 +0000

    both sonars are now independent, but both need the yaml file.

--- copy_eurathlon_vm.sh
+++ copy_eurathlon_vm.sh
@@ -5,7 +5,7 @@ rsync -avzh ./src/*.cpp 							eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_r
 rsync -avzh ./include/big_red_launch_configs/*.hpp 	eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/include/big_red_launch_configs/
 rsync -avzh CMakeLists.txt 							eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
 rsync -avzh *.xml 									eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
-rsync -avzh *.launch	 							eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
+rsync -avzh ./launch/*.launch	 							eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/launch
 rsync -avzh *.yaml		 							eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
 rsync -avzh *.md									eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/
 rsync -avzh ./urdf/*.urdf							eurathlon_vm:/home/euratlhon/uwesub_msc/src/big_red_launch_configs/urdf/
