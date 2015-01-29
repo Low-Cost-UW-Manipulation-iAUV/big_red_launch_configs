ab2eb3cc2123335c08dd87191293af039a7d5337
Raphael Nagel <compiaffe@users.noreply.github.com>
2015-01-29 18:10:55 +0000

    using the udev rules defined serial ports

--- copy_support_micron_driver_bmt.sh
+++ copy_support_micron_driver_bmt.sh
@@ -5,7 +5,7 @@ rsync -avzh ../support_auv/micron_driver/src/*.cpp 								bmt:/home/devel/catki
 rsync -avzh ../support_auv/micron_driver/include/micron_driver/*.hpp 			bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/include/micron_driver/
 rsync -avzh ../support_auv/micron_driver/CMakeLists.txt 						bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/
 rsync -avzh ../support_auv/micron_driver/*.xml 									bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/
-rsync -avzh ../support_auv/micron_driver/*.launch	 							bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/
+rsync -avzh ../support_auv/micron_driver/launch/*.launch	 							bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/launch
 rsync -avzh ../support_auv/micron_driver/*.yaml		 							bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/
 rsync -avzh ../support_auv/micron_driver/*.md									bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/
 rsync -avzh ../support_auv/micron_driver/urdf/*.urdf							bmt:/home/devel/catkin_ws/src/support_auv/micron_driver/urdf/
